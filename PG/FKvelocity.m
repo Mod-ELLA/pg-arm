6343887a036cdb968b99a09f007c946e7c800660
fff-doudou <fff.doudou@gmail.com>
2014-10-31 18:14:47 -0400

    FKvelocity.m Implemented

--- PG/FKvelocity.m
+++ PG/FKvelocity.m
@@ -1,6 +1,16 @@
-function V = FKvelocities(angles, lengths, angularVelocities)
+function V = FKvelocity(angles, lengths, angularVelocities)
 % Need to compute the Jacobian
 % Given joint angles, linkage lengths, and angular velocities
 % (rad/time) compute the end effector velocity V
+% Return the end effector velocity V in a 2-by-1 vector
+%%
+numOfArms = length(lengths);
+tempV = [0;0];
+for i = 1:numOfArms
+    relativeVel = [-lengths(i)*angularVelocities(i)*sind(angles(i));
+                    lengths(i)*angularVelocities(i)*cosd(angles(i))];
+    tempV = tempV+relativeVel;
+end
+V = tempV;
 
 end
