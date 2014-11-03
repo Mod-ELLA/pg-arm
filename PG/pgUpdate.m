0f9b3dcf5901a97cb027292c660435b6d716c535
Edward Cui <princealva@hotmail.com>
2014-11-02 20:32:00 -0500

    Finished the pgUpdate function and added a new input argument

--- PG/pgUpdate.m
+++ PG/pgUpdate.m
@@ -1,3 +1,14 @@
-function M = pgUpdate(M, grad)
+function M_t_plus_1 = pgUpdate(M_t, grad, step)
 % Update the Model given the gradient
+% M_t           : a model matrix, m by n
+% grad          : a vector of gradient of J at theta, nm by 1
+% step          : update step size
+% M_t_plus_1    : updated model matrix, m by n
+if nargin == 2
+    step = 0.001;
+end
+m = size(M_t,1);
+n = size(M_t,2);
+M_t_plus_1 = reshape(M_t',1,[]) + step*grad';
+M_t_plus_1 = reshape(M_t_plus_1, n, m)';
 end
