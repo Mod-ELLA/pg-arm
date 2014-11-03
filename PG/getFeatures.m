e7ff13e37fbe652abf6f2a35bca16842afe1739c
Edward Cui <princealva@hotmail.com>
2014-11-02 21:43:18 -0500

    Finished the feature extraction function and modified pgUpdate function.

--- PG/getFeatures.m
+++ PG/getFeatures.m
@@ -1,3 +1,9 @@
 function features = getFeatures(state)
 % Returns a nx1 vector of feature values given the state
+% In this function, the features are:
+% joint angles; sine of joint angles; cosine of joint angles;
+% tangent of joint angle; product of sine and cosine of the joint angles
+joint_angles = reshape(state.angles,[],1);
+features = joint_angles;
+features = [features; sin(joint_angles); cos(joint_angles); tan(joint_angles); sin(joint_angles).*cos(joint_angles)];
 end
--- PG/pgUpdate.m
+++ PG/pgUpdate.m
@@ -1,14 +1,11 @@
 function M_t_plus_1 = pgUpdate(M_t, grad, step)
 % Update the Model given the gradient
-% M_t           : a model matrix, m by n
-% grad          : a vector of gradient of J at theta, nm by 1
-% step          : update step size, a positive scalar
-% M_t_plus_1    : updated model matrix, m by n
+% M_t           : a model matrix; m by n
+% grad          : a matrix of gradient of J at theta_t; m by n
+% step          : update step size; a positive scalar
+% M_t_plus_1    : updated model matrix; m by n
 if nargin == 2
     step = 0.001;
 end
-m = size(M_t,1);
-n = size(M_t,2);
-M_t_plus_1 = reshape(M_t',1,[]) + step*grad';
-M_t_plus_1 = reshape(M_t_plus_1, n, m)';
+M_t_plus_1 = M_t + step*grad;
 end
