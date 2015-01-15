a938878aaaf5d931e4363d7c90b224b9fd15c9fa
Levi Cai <cail@seas.upenn.edu>
2015-01-15 17:18:57 -0500

    Deleted vishnu's code from here

--- ELLASub/doAction.m
+++
@@ -1,8 +0,0 @@
-function  [r,stateNext] = doAction(type,state,action, param)
-
-    if strcmpi(type,'mc')==1
-        
-        [r,stateNext] = next_state_mc(state,action,param.posBounds,param.posGoal,param.velBounds,param.slope,param.rewardMult);
-    end
-
-end
